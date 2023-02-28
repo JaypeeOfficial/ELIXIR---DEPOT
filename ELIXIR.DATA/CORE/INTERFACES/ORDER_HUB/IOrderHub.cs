d54bed8801544a5a979a844e43ea19a5f1528abe
Aldrin Vega <aldrinmundovega@gmail.com>
2023-02-28 13:17:59 +0800

    Checklist and Adjustment in Allocation

---
+++ ELIXIR.DATA/CORE/INTERFACES/ORDER_HUB/IOrderHub.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ELIXIR.DATA.DATA_ACCESS_LAYER.MODELS.ORDERING_MODEL;
+
+namespace ELIXIR.DATA.CORE.INTERFACES.ORDER_HUB
+{
+    public interface IOrderHub
+    {
+        Task SetBeingPrepared(List<Ordering> moveList);
+    }
+}
---
+++ ELIXIR.DATA/SERVICES/OrderHub.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ELIXIR.DATA.CORE.INTERFACES.ORDER_HUB;
+using ELIXIR.DATA.DATA_ACCESS_LAYER.MODELS.ORDERING_MODEL;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ELIXIR.DATA.SERVICES
+{
+    public class OrderHub : Hub<IOrderHub>
+    {
+        public async Task SetBeingPrepared(List<Ordering> moveList)
+        {
+            await Clients.Others.SetBeingPrepared(moveList);
+        }
+      
+    }
+} 
